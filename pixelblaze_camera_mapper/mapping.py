39d418e504f0a5722ee95596c59e82bdc09f2e08
Sarah van der Bleek <sarah@vanderbleek.com>
2024-04-14 16:49:19 -0700

    Finish implementation of binary mapping

--- pixelblaze_camera_mapper/mapping.py
+++ pixelblaze_camera_mapper/mapping.py
@@ -2,8 +2,10 @@
 import time
 import json
 import sys
+from typing import NamedTuple
 
 import pixelblaze
+from scipy.spatial import distance
 
 import config
 import camera
@@ -42,7 +44,7 @@ def show_calibration_pixels(pb, capture_multiple_pixels):
     if capture_multiple_pixels:
         print("Lighting calibration pixels")
         pb.setActivePatternByName("binary mapping")
-        calibration_array = [1, 0] * (int)(config.NUM_LEDS / 2)
+        calibration_array = [1] * (int)(config.NUM_LEDS)
         pb.setActiveVariables({"pixels_to_light": calibration_array})
     else:
         print("Blinking calibration pixel")
@@ -53,6 +55,64 @@ def show_calibration_pixels(pb, capture_multiple_pixels):
 def all_pixels_off(pb):
     pb.setActivePatternByName("pixel index")
     pb.setActiveVariables({"pixel_to_light": -1})
+
+
+def get_num_bits_for_num_leds():
+    return int(config.NUM_LEDS).bit_length()
+
+
+class LedWithFramesSeen(NamedTuple):
+    coordinate: list
+    frames_seen: dict
+
+
+def find_known_leds_in_frame(all_known_leds, leds_in_frame, frame_index):
+    # Handle case where no LEDs were found this frame
+    if len(leds_in_frame) < 1:
+        for led in all_known_leds:
+            led.frames_seen[frame_index] = False
+        return
+
+    for led in all_known_leds:
+        # LED center might not be exactly the same, so let's find which known LED is close
+        distance_array = distance.cdist([led.coordinate], leds_in_frame, "euclidean")
+        closest_pixel_index = distance_array.argmin()
+        if distance_array[0][closest_pixel_index] < 2:
+            led.frames_seen[frame_index] = True
+        else:
+            led.frames_seen[frame_index] = False
+
+
+def capture_locations_of_leds_for_led_string_state(
+    pb, light_pattern, vc, threshold, background_image, frame_number
+):
+    # pb.setActivePatternByName("binary mapping")
+    pb.setActiveVariables({"pixels_to_light": light_pattern})
+    time.sleep(0.5)
+    frame = camera.get_frame(vc, background_image)
+    current_locations, _, _ = camera.get_led_positions(
+        frame,
+        threshold,
+        find_multiple_leds=True,
+        save_image=True,
+        minimum_dimension=1,
+        frame_number=frame_number,
+    )
+    return current_locations
+
+
+def get_led_index_from_frames_seen(binary_dict):
+    binary_array = [str(0)] * get_num_bits_for_num_leds()
+    for key, value in binary_dict.items():
+        if value:
+            binary_array[key] = str(1)
+        else:
+            binary_array[key] = str(0)
+
+    binary_array_little_endian = reversed(binary_array)
+    binary_string = "".join(binary_array_little_endian)
+    decimal_integer = int(binary_string, 2)
+    return decimal_integer
 
 
 def map_pixels_binary(pb):
@@ -66,30 +126,52 @@ def map_pixels_binary(pb):
     vc = camera.open_camera(config.CAMERA_ID)
     pb.setActivePatternByName("binary mapping")
     print("Starting LED location capture")
-    num_bits = int(config.NUM_LEDS).bit_length()
 
-    locations = []
+    # Find locations of all pixels first
+    turn_all_pixels_on = [1] * config.NUM_LEDS
+    locations_of_all_leds = capture_locations_of_leds_for_led_string_state(
+        pb=pb,
+        light_pattern=turn_all_pixels_on,
+        vc=vc,
+        threshold=threshold,
+        background_image=background_image,
+        frame_number=9999,
+    )
+
+    # Initialize list of known LED positions
+    all_known_leds = []
+    for led in locations_of_all_leds:
+        new_led_def = LedWithFramesSeen(coordinate=led, frames_seen={})
+        all_known_leds.append(new_led_def)
+
+    # Capture each frame of LEDs in the binary pattern
+    num_bits = get_num_bits_for_num_leds()
     for i in range(num_bits):
+        # Turn on pixels in the pattern, find their coordinates
         pixel_array = set_binary_pattern(num_bits, i)
-        pb.setActiveVariables({"pixels_to_light": pixel_array})
-        time.sleep(0.5)
-        frame = camera.get_frame(vc, background_image)
-        current_locations, _, _ = camera.get_led_positions(
-            frame,
-            threshold,
-            find_multiple_leds=True,
-            save_image=True,
-            minimum_dimension=1,
+        leds_found_in_frame = capture_locations_of_leds_for_led_string_state(
+            pb=pb,
+            light_pattern=pixel_array,
+            vc=vc,
+            threshold=threshold,
+            background_image=background_image,
             frame_number=i,
         )
-        locations.append(current_locations)
+        # Reconcile what we found in this frame with the LEDs we know about
+        find_known_leds_in_frame(all_known_leds, leds_found_in_frame, i)
 
-    # TODO: Now that we have many arrays of coordinates, we need to reconcile the blobs
-    # and locate the pixels based on their inferred id
-    print(locations)
-    print("Binary mapping not yet fully implemented, sorry.")
+    print("Done capturing LED locations with camera")
+    vc.release()
 
-    return None
+    print("Turning all LED locations into an LED array")
+    led_array = [
+        [-1] * 2 for i in range(config.NUM_LEDS)
+    ]  # Initialize array with all [-1,-1]
+    for led in all_known_leds:
+        pixel_index = get_led_index_from_frames_seen(led.frames_seen)
+        led_array[pixel_index] = led.coordinate
+
+    return ignore_empty_pixels(led_array)
 
 
 def map_pixels_linearly(pb):
